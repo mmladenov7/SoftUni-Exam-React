d4e94196ad11760f04b771e48484cdc2e07ec5e2
mmladenov7 <mladenovm1008@gmail.com>
2023-12-09 00:14:25 +0200

    Added login/register data verification

--- client/src/contexts/AuthContext.jsx
+++ client/src/contexts/AuthContext.jsx
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 
 import apiFetch from "../api";
 import usePersistedState from "../hooks/usePersistedState";
-import { dataChecker, errorThrower } from "../utils/utils";
+import { dataChecker, errorThrower, notLongEnoughError } from "../utils/utils";
 
 import ErrorContext from "./ErrorContext";
 
@@ -20,15 +20,38 @@ export function AuthProvider({ children }) {
         dataChecker(data)
 
         if (data.email.length < 8) {
-            errorThrower('email must be at least 8 characters long')
+            notLongEnoughError('Email', 8)
         }
 
         if (data.password.length < 5) {
-            errorThrower('password must be at least 5 characters long')
+            notLongEnoughError('Password', 5)
         }
 
         return true
     }
+
+    function validateRegister(data) {
+        dataChecker(data)
+
+        if (data.username.length < 4) {
+            notLongEnoughError('Username', 4)
+        }
+
+        if (data.email.length < 8) {
+            notLongEnoughError('Email', 8)
+        }
+
+        if (data.password.length < 5) {
+            notLongEnoughError('Password', 5)
+        }
+
+        if (data.password != data.repeatPassword) {
+            errorThrower('Passwords must match')
+        }
+
+        return true
+    }
+
 
     async function authenticateUser(e, data, path) {
         e.preventDefault()
@@ -38,18 +61,21 @@ export function AuthProvider({ children }) {
             if (path == 'login') {
                 validateLogin(data)
             } else if (path == 'register') {
-
+                validateRegister(data)
             }
 
             const response = await apiFetch.post(`users/${path}`, data)
             if (!response.ok) {
+
+                //TODO
+                
                 console.log("error")
             } else {
                 const result = await response.json()
                 setUser(result)
 
                 hideError()
-                
+
                 navigate("/")
             }
 
--- client/src/utils/utils.js
+++ client/src/utils/utils.js
@@ -2,10 +2,14 @@ export const errorThrower = function (message) {
     throw new Error(message)
 }
 
+export const notLongEnoughError = function (field, len) {
+    errorThrower(`${field} must be at least ${len} characters long`)
+}
+
 export const dataChecker = function (data) {
     for (const [key, value] of Object.entries(data)) {
         if (!value) {
-            errorThrower(`${key} is required`)
+            errorThrower(`${key[0].toUpperCase() + key.substring(1)} is required`)
         }
     }
     return true
